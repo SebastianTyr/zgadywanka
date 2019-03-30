f92b267b7417c9da3e949a18816f297b3f8dbd1a
SebastianTyr <sebastian.tyralski@microsoft.wsei.edu.pl>
2019-03-30 18:51:42 +0100

    Jedna rozgrywka

--- GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -10,11 +10,37 @@ namespace GraMonolitycznie
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Witaj");
             Console.Write("Podaj swoje imię: ");
             string x = Console.ReadLine();
             Console.WriteLine($"Witaj {x}");
-            Console.ReadKey();
+            */
+
+            //1. Komputer losuje liczbę
+            Random generator = new Random();
+            int wylosowana = generator.Next(1, 101);
+            Console.WriteLine("Wylosowałem liczbę od 1 do 100. \nOdgadnij ją");
+
+#if(DEBUG)
+            Console.WriteLine(wylosowana);
+#endif
+
+            //2. Człowiek proponuje rozwiązanie
+            Console.Write("Podaj swoją propozycję: ");
+            string tekst = Console.ReadLine();
+            int propozycja = Convert.ToInt32(tekst);
+            Console.WriteLine($"Podałeś wartość {propozycja}");
+
+            //3. Komputer ocenia propozycję
+            if (propozycja < wylosowana)
+                Console.WriteLine("Za mało");
+            else if (propozycja > wylosowana)
+                Console.WriteLine("Za dużo");
+            else
+                Console.WriteLine("Trafiono");
+
+            Console.WriteLine("Koniec gry");
         }
     }
 }
